59ac179b09ca8defa0de6c50df3b4721fae178e1
Connor <resistingprawn5@gmail.com>
2024-11-19 14:44:55 -0700

    i think im done

--- gameresults.py
+++ gameresults.py
@@ -8,16 +8,31 @@ from scraper import get_team_record
 import time
 
 def clean_team_name(team_name):
-    team_name = re.sub(r'\s+', '', team_name)
+    team_name = re.sub(f'-', '', team_name) #hyphen
+    team_name = re.sub(r'\s+', '', team_name) #whitespace
+    team_name = re.sub(r'\(\d+\)', '', team_name).strip() #strip ranking
     if team_name == "Miami(FL)":
         team_name = "MiamiFlorida"
     elif team_name == "Miami(OH)":
         team_name = "MiamiOhio"
     elif team_name == "KentState":
         team_name = "Kent"
+    elif team_name == "Texas-El Paso":
+        team_name = "TexasElPaso"
+    elif team_name == "Louisiana": 
+        team_name = "LouisianaLafayette"
+    elif team_name == "BowlingGreen":
+        team_name = "BowlingGreenState"
+    
+    
+
     return team_name
 
+with open('2000seasonWithWins', 'r') as myjson:
+    football_data = json.load(myjson)
 
+fbs_teams = {node["id"] for node in football_data["nodes"]}
+print(fbs_teams)
 
 
 def scrape_cfb_results():
@@ -47,14 +62,20 @@ def scrape_cfb_results():
         winner = clean_team_name(cols[3].text.strip())
         winner_points = clean_team_name(cols[4].text.strip())
         loser = clean_team_name(cols[6].text.strip())
+        print(cols[6].text.strip())
         loser_points = clean_team_name(cols[7].text.strip())
         #print(loser_points)
 
         #get rid of the school's rankings
         formatted_winner = re.sub(r'\(\d+\)', '', winner).strip()
         formatted_loser = re.sub(r'\(\d+\)', '', loser).strip()
-        print(formatted_winner)
+        print(formatted_loser)
+        #print(formatted_winner)
         #print(formatted_loser)
+
+        #used for checking if the teams are both fbs teams
+        if formatted_winner not in fbs_teams or formatted_loser not in fbs_teams:
+            continue
 
         #handle missing or invalid data
         if not formatted_winner or not formatted_loser:
@@ -73,7 +94,7 @@ def scrape_cfb_results():
 
 #get the JSON output
 json_results = scrape_cfb_results()
-print(json_results)
+#print(json_results)
 parsed_results = json.loads(json_results)
 #print(json_results)
 
@@ -137,8 +158,8 @@ for scraped_edge in parsed_results:
   
         #**edge[2]
     })
-    print(scraped_edge["source"])
-    print(scraped_edge['target'])
+    #print(scraped_edge["source"])
+    #print(scraped_edge['target'])
 
 #turn to json
 output_json = {
